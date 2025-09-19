f1f604ba375e593d6755acc6ee16a16a9d3a16ab
belalR666 <render27091848@gmail.com>
2025-09-19 19:43:05 +0600

    Update sadvide.js

--- Script/commands/sadvide.js
+++ Script/commands/sadvide.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
- var hi = ["হাসি মুখে লুকিয়ে রাখি,\nচোখের জলে লিখে যাই গল্প…!😅💔"];
+ var hi = ["হাসি মুখে লুকিয়ে রাখি,\nচোখের জলে লিখে যাই গল্প…!😅 𒀱⃝⃞⃟✡️🅱🅴🅻🅰🅻 🅱🅾🆃❤⃝⃞⃟𒀱ꪳ💔"];
  var know = hi[Math.floor(Math.random() * hi.length)];
  var link = [
 
