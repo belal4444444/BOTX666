9b8f4e5907ae17c7c71643226bba3b1c996fcd4f
BOTX666 <botx6666@gmail.com>
2025-09-28 00:36:44 +0600

    Update and rename Belalam.js to Belalam2.js

--- Script/commands/Belalam2.js
+++ Script/commands/Belalam2.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const request = require("request");
 
 module.exports.config = {
- name: "ampp",
+ name: "am2",
  version: "1.0.1",
  hasPermssion: 0,
  credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐒𝐀𝐇𝐔",
