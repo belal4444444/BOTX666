128c4aa45dd4a3394b0826a03429e44aaeb44313
BOTX666 <botx6666@gmail.com>
2025-10-13 21:17:39 +0600

    Update owner2.js

--- Script/commands/owner2.js
+++ Script/commands/owner2.js
@@ -5,7 +5,7 @@ module.exports.config = {
  name: "owner2",
  version: "1.0.1",
  hasPermssion: 0,
- credits: "Shahadat SAHU",
+ credits: "BELAL BOTX666",
  description: "Display bot owner's information",
  commandCategory: "Info",
  usages: "",
