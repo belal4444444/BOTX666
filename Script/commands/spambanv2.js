7898674e9a6ee59a6c888dd099f80551bac3f9ba
BOTX666 <botx6666@gmail.com>
2025-10-31 19:52:27 +0600

    Update and rename sspamban.js to spambanv2.js

--- Script/commands/spambanv2.js
+++ Script/commands/spambanv2.js
@@ -1,7 +1,7 @@
  const num = 5 //number of times spam gets banned -1, for example 5 times 6 times will get banned
  const timee = 20 // During `timee` spam `num` times will be banned
  module.exports.config = {
-  name: "sspamban",
+  name: "spambanv2",
   version: "2.0.0",
   hasPermssion: 0,
   credits: "NTKhang", //fix get by  D-Jukie
